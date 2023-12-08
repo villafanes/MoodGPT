cdba03037a6cc5bb42383d6ec1923ec999a7075d
villafanes <117455242+villafanes@users.noreply.github.com>
2023-12-07 21:12:19 -0500

    Update genius_api.py

--- genius_api.py
+++ genius_api.py
@@ -1,22 +1,30 @@
+import requests
 from lyricsgenius import Genius
 
-api_key =  '4kcHpLNfV65xVafDjTSaTpw45hD-Gu7B2M7LocpNTyUlAkbxg5ooFWd16brLf0mP'
-genius = Genius(api_key)
-artist = input('Enter artist name: ')
-title = input('Enter song title: ')
-song = genius.search_song(title,artist)
+def get_lyrics(artist, title):
+    api_key = '4kcHpLNfV65xVafDjTSaTpw45hD-Gu7B2M7LocpNTyUlAkbxg5ooFWd16brLf0mP'
+    genius = Genius(api_key)
+    try:
+        song = genius.search_song(title, artist)
+        if song:
+            # array of every line of lyrics
+            lyrics = song.lyrics
+            # remove empty elements
+            lyrics = lyrics.split('\n')
 
-#array of every line of lyrics
-lyrics = song.lyrics
-lyrics = lyrics.split('\n')
+            for i in lyrics:
+                if i == '':
+                    lyrics.remove(i)
 
-#remove empty elements
-for i in lyrics:
-    if i == '':
-        lyrics.remove(i)
+            # remove headings
+            for i in lyrics:
+                for j in i:
+                    if j == '[':
+                        lyrics.remove(i)
 
-#remove headings
-for i in lyrics:
-    for j in i:
-        if j == '[':
-            lyrics.remove(i)
+            return song.lyrics
+        else:
+            return None
+    except requests.exceptions.Timeout:
+        print("Request to Genius_API timed out. Try running the program again.")
+        return None
