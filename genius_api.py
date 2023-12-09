d6476dedcc6e4c90a3d8b866c7d3b19f1a30a97e
villafanes <117455242+villafanes@users.noreply.github.com>
2023-12-09 10:04:25 -0500

    Update genius_api.py

--- genius_api.py
+++ genius_api.py
@@ -5,10 +5,10 @@ def get_lyrics(artist, title):
     api_key = '4kcHpLNfV65xVafDjTSaTpw45hD-Gu7B2M7LocpNTyUlAkbxg5ooFWd16brLf0mP'
     genius = Genius(api_key)
 
-    #remove extraneous outputs
+    # remove extraneous outputs
     genius.remove_section_headers = True
     genius.verbose = False
-    
+
     try:
         song = genius.search_song(title, artist)
         if song:
@@ -21,14 +21,13 @@ def get_lyrics(artist, title):
                 if i == '':
                     lyrics.remove(i)
 
-            #record artist and title without typos
             artist_used = song.artist
             song_used = song.title
-
-            return lyrics
+            return lyrics, artist_used, song_used
         else:
-            print("Song cannot be found.")
-            return None
+            return None, None, None
+
     except requests.exceptions.Timeout:
         print("Request to Genius_API timed out. Try running the program again.")
-        return None
+        return None, None, None
+
